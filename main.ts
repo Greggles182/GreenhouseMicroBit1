30146dd3393fce61e6823e34d1acee5cd1d596e4
Greggles182 <76397590+Greggles182@users.noreply.github.com>
2021-06-12 19:36:13 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 enum RadioMessage {
     message1 = 49434,
-    LightOn = 49786
+    LightOn = 49786,
+    LightOff = 64509,
+    HeaterOn = 29523,
+    HeaterOff = 54869
 }
 let temp = 0
 let light2 = 0
@@ -13,4 +16,7 @@ basic.forever(function () {
     basic.showNumber(light2)
     basic.showString("Temperature (°C): ")
     basic.showNumber(temp)
+    radio.sendMessage(RadioMessage.HeaterOn)
+    radio.sendMessage(RadioMessage.LightOn)
+    basic.pause(1000)
 })
