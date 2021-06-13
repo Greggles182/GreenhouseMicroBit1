fb338d51dd94132f49dd276ed48066f9cccba057
Greggles182 <76397590+Greggles182@users.noreply.github.com>
2021-06-13 07:24:03 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,8 +3,24 @@ enum RadioMessage {
     LightOn = 49786,
     LightOff = 64509,
     HeaterOn = 29523,
-    HeaterOff = 54869
+    HeaterOff = 54869,
+    WaterOn = 6289
 }
+input.onPinPressed(TouchPin.P0, function () {
+    radio.sendMessage(RadioMessage.HeaterOn)
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendMessage(RadioMessage.LightOff)
+})
+input.onPinPressed(TouchPin.P2, function () {
+    radio.sendMessage(RadioMessage.WaterOn)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendMessage(RadioMessage.HeaterOff)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    radio.sendMessage(RadioMessage.LightOn)
+})
 let temp = 0
 let light2 = 0
 let RadioGroup = 1
@@ -16,7 +32,5 @@ basic.forever(function () {
     basic.showNumber(light2)
     basic.showString("Temperature (°C): ")
     basic.showNumber(temp)
-    radio.sendMessage(RadioMessage.HeaterOn)
-    radio.sendMessage(RadioMessage.LightOn)
     basic.pause(1000)
 })
